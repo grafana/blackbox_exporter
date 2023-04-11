199f87f40ef6350ba45d8aa8935aa156716bed9c
Marc Tuduri <marctc@protonmail.com>
2023-04-11 14:22:32 +0200

    remove metrics

--- config/config.go
+++ config/config.go
@@ -39,12 +39,6 @@ import (
 )
 
 var (
-	configReloadSuccess = promauto.NewGauge(prometheus.GaugeOpts{
-		Namespace: "blackbox_exporter",
-		Name:      "config_last_reload_successful",
-		Help:      "Blackbox exporter config loaded successfully.",
-	})
-
 	configReloadSeconds = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "blackbox_exporter",
 		Name:      "config_last_reload_success_timestamp_seconds",
@@ -103,9 +97,7 @@ func (sc *SafeConfig) ReloadConfig(confFile string, logger log.Logger) (err erro
 	var c = &Config{}
 	defer func() {
 		if err != nil {
-			configReloadSuccess.Set(0)
 		} else {
-			configReloadSuccess.Set(1)
 			configReloadSeconds.SetToCurrentTime()
 		}
 	}()
